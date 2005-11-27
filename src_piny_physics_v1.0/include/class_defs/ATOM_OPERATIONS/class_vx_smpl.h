8373c9cedd2c0af68e72eff38633742b6e904be3
Glenn Martyna <martyna@us.ibm.com>
2005-11-27 01:37:09 +0000

    more dynamics

---
+++ src_piny_physics_v1.0/include/class_defs/ATOM_OPERATIONS/class_vx_smpl.h
@@ -0,0 +1,70 @@
+//=================================================================== 
+//ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+//=================================================================== 
+//                                                                    
+// Class for velocity resampling                            
+//                                                                    
+//=================================================================== 
+
+#ifndef _VX_SMPL_
+#define _VX_SMPL_
+
+#include "standard_include.h"
+#include "../../mathlib/proto_math.h"
+#include "../../../include/Atoms.h"
+
+//=================================================================== 
+//ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+//=================================================================== 
+class VX_SMPL{
+  public: 
+     VX_SMPL() {}
+    ~VX_SMPL() {}
+     static void ctrlSamplAtomVel(
+             int     natm,       // Number of atoms in system
+             double* velx,       // x-comp of atom velocities
+             double* vely,       // y-comp of atom velocities
+             double* velz,       // z-comp of atom velocities
+             double* mass        // Array containting atom masses
+     );
+     static void ctrlSamplAtomVel(
+             int     natm,       // Number of atoms in system
+             Atom*   atoms  
+     );
+     static void ctrlSamplAtomNhcVel(
+             int      natm,        // Number of atoms in system
+             AtomNHC *atomsNHC
+     );
+     static void sampl3DVelMultiT(
+                         int     natm,
+                         double* velx,  
+                         double* vely,  
+                         double* velz,  
+                         double* mass,
+                         double* text_atm,
+                         int*    iseed,
+                         int*    iseed2,
+                         double* qseed
+     );
+     static void sampl3DVelMultiT(
+                         int     natm,
+                         Atom *atoms,
+                         double* text_atm,
+                         int*    iseed,
+                         int*    iseed2,
+                         double* qseed
+     );
+     static void sampl3DVelOneT(
+                         int natm, 
+                         double* vx, 
+                         double* vy,
+                         double* vz,
+                         double* mass,
+                         double  kT,
+                         int* iseed,
+                         int* iseed2,
+                         double* qseed
+    );
+};
+//=================================================================== 
+#endif  
