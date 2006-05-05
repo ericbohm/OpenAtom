7801a718301068eb7886fc57a5c4060af67b1f74
Abhinav S Bhatele <bhatele@illinois.edu>
2006-05-05 19:21:57 +0000

    added documentation and renamed some functions

--- src_charm_driver/load_balance/FindProcessor.h
+++ src_charm_driver/load_balance/FindProcessor.h
@@ -1,10 +1,11 @@
 /** \file FindProcessor.h
- * Finds the nearest processor from a given processor in different topologies.
- *
+ *  Author: Abhinav S Bhatele
+ *  Date Created: November 30th, 2005
+ *  Finds the nearest processor from a given processor in different topologies.
  */
+
 #ifndef _FINDPROCESSOR_H
 #define _FINDPROCESSOR_H
-
 
 #include <math.h>
 #include <iostream.h>
@@ -12,28 +13,28 @@
 class FindProcessor 
 {
 	public:
-		int option;
-		int nop;
-		int nopX, nopY, nopZ;
-		int start[3];
-		int next[3];
-		int w;
-		int count;
+		int option;		// input about which topology is it
+		int nop;		// total no. of processors
+		int nopX, nopY, nopZ;	// processors in X, Y and Z dimension respectively 
+		int start[3];		// starting processor
+		int next[3];		// next processor w.r.t start
+		int w;			// the fourth dimension for bluegene in virtual node mode
+		int count;		// no. of processors output till now 
 	public:
-		FindProcessor();
-		FindProcessor(int a[]);
-		int main(int argc, char*argv[]);
-		void findNext(int a[]);
-		int findNextInBluegene(int a[]);
-		int findNextInBIter(int a[]);
-		int findNextIter(int a[]);
-		int findNextInTorus(int a[]);
-		int findNextInTorusV(int t, int a[]);
-		void printSome(int n);
+		FindProcessor();			// default constructor
+		FindProcessor(int a[]);			// set start[] to a[] 
+		void findNext(int a[]);			// for infinite no. of processors in 3D
+		int findNextInMIter(int a[]);		// helper function for 3D mesh
+		int findNextInMesh(int a[]);		// for 3D mesh
+		int findNextIter(int a[]);		// helper funtion for 3D torus
+		int findNextInTorus(int a[]);		// for 3D torus in CO mode
+		int findNextInTorusV(int t, int a[]);	// for 3D torus in VN mode
+		int compare(int n, int a, int b);	// helper function
+		void printSome(int n);			
 		void printing(int a, int b, int c);
 		void printing_sp(int a, int b, int c);
 		void printing(int w, int a, int b, int c);
-		int compare(int n, int a, int b);
+		//int main(int argc, char*argv[]);
 };
 
 #endif
