ae905cc5d6c46d188ca9ccc5f3e38bc0c2093fee
Abhinav S Bhatele <bhatele@illinois.edu>
2006-12-12 17:36:45 +0000

    set splitting on by default on bluegene

--- src_charm_driver/paircalc/ckPairCalculator.h
+++ src_charm_driver/paircalc/ckPairCalculator.h
@@ -26,10 +26,8 @@
 //#define TEST_ALIGN
 #define BUNDLE_USER_EVENT  
 
-//#define PC_FWD_DGEMM_SPLIT 16   //multiple of 6 for BG/L?  use 16 for happier align 
-//#define PC_BWD_DGEMM_SPLIT 16
-#define PC_FWD_DGEMM_SPLIT 0
-#define PC_BWD_DGEMM_SPLIT 0
+#define PC_FWD_DGEMM_SPLIT 16   //multiple of 6 for BG/L?  use 16 for happier align 
+#define PC_BWD_DGEMM_SPLIT 16
 
 #else
 #define PC_FWD_DGEMM_SPLIT 0 
