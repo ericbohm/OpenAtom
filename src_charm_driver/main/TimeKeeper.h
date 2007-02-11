fe27315dccfea338b7f2056b7a9ad4e4df8d2991
Eric Bohm <ebohm@illinois.edu>
2007-02-11 16:19:19 +0000

    chare to record times per sub step phase

---
+++ src_charm_driver/main/TimeKeeper.h
@@ -0,0 +1,57 @@
+/**
+ *
+ *  A place to collect substep times.  Each participating chare will
+ *  register with the timekeeper on processor 0.  Giving its name and
+ *  getting the next available timekeeper id.
+ *  
+ *  Then at the beginning of its notional step every chare will
+ *  contribute its start time to a minimum reduction.
+ *
+ *  At the end of its notional step each chare will contribute its end
+ *  time to a maximum reduction.
+ *
+ *  The timekeeper will print out the delta time and name for each substep
+ *  when it has the min and max.
+ */
+#include <vector>
+#include <string>
+extern int TimeKeeperID;
+extern vector <string> TimeKeeperNames;
+int keeperRegister(string name)
+{
+  
+  TimeKeeperNames.push_back(name);
+  return(TimeKeeperID++);
+}
+
+class TimeKeeper : public Chare
+{
+ public:
+  double *beginTime;
+  TimeKeeper(){
+  }
+  void init ()
+    {
+      beginTime=new double[TimeKeeperID+1];
+    }
+  void collectStart(CkReductionMsg *msg)
+    {
+      int phase= msg->getUserFlag();
+      CkAssert(phase<TimeKeeperID);
+      CkAssert(phase>=0);
+      beginTime[phase]  = *((double *)msg->getData());
+      //      CkPrintf("Phase %s start %.10g\n",TimeKeeperNames[phase].c_str(), beginTime[phase]);
+      delete msg;
+    }
+
+  // we assume sanity and do our printing when each max arrives
+  void collectEnd(CkReductionMsg *msg)
+    {
+      int phase = msg->getUserFlag();
+      CkAssert(phase<TimeKeeperID);
+      double endTime  = *((double *)msg->getData());
+      delete msg;
+      //      CkPrintf("Phase %s start %.10g end %.10g duration %.10g\n",TimeKeeperNames[phase].c_str(),beginTime[phase],endTime, endTime-beginTime[phase]);
+      CkPrintf("Phase %s duration %.10g\n",TimeKeeperNames[phase].c_str(), endTime-beginTime[phase]);
+    }
+};
