6e20ad2472dfea2a31a5a7c49e01ebaa16e38ddc
Ramprasad Venkataraman <ramv@illinois.edu>
2009-02-03 00:45:39 +0000

    Missed a file during the prev commit for task "implement a GSpaceDriver chare"

---
+++ src_charm_driver/structure_factor/StructureFactorMessages.h
@@ -0,0 +1,33 @@
+#include "structureFactor.decl.h"
+#include "structureFactorCache.decl.h"
+
+#ifndef STRUCTURE_FACTOR_MESSAGES_H
+#define STRUCTURE_FACTOR_MESSAGES_H
+
+
+class SFDummyMsg: public CMessage_SFDummyMsg {
+ public:
+  int iteration_src;
+};
+
+
+
+class PPDummyMsg: public CMessage_PPDummyMsg {
+ public:
+  int atmGrp;
+  int sfindex;
+};
+
+
+
+class StructFactorMsg: public CkMcastBaseMsg, public CMessage_StructFactorMsg {
+ public:
+	int datalen;
+	int atmGrpIndex;
+	int gsSize;
+	int planeIndex; 
+	complex *structFactor; 
+	complex *structFactor_fx, *structFactor_fy, *structFactor_fz;
+};
+
+#endif // STRUCTURE_FACTOR_MESSAGES_H
