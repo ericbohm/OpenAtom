5b895ce01deb8762dc2872000a2566b4584860f0
Abhinav S Bhatele <bhatele@illinois.edu>
2009-04-02 22:14:52 +0000

    *** empty log message ***

--- src_piny_physics_v1.0/include/charm_defs/Interface_ctrl.decl.h
+++ src_piny_physics_v1.0/include/charm_defs/Interface_ctrl.decl.h
@@ -1,8 +1,5 @@
 #ifndef _DECL_Interface_ctrl_H_
 #define _DECL_Interface_ctrl_H_
 #include "charm++.h"
-#ifdef CMK_CHECK_CRC
-extern "C" void checkAllCRC(int);
-#endif
 extern void _registerInterface_ctrl(void);
 #endif
