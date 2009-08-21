023a06e5e6d08667fdd629cc7652a52d5b3da856
Ramprasad Venkataraman <ramv@illinois.edu>
2009-08-21 13:45:08 -0500

    Fixes to make sure the build directory can be located anywhere.
    User can specify where=some/path on the make command line. The build directory is also now renamed to build (from binary/)

--- Makefile
+++ Makefile
@@ -1,15 +1,27 @@
-# Define the base, build and include directories of the openatom tree
+# Define the base directory and get the openatom tree
 base   := .
 makedir = $(base)/compile
 include $(makedir)/srcdirs.mk
 
-# Define the command line args to sub-makes
-MAKEARGS = -C $(build) -f ../$(makedir)/Makefile
+################## Build directory related stuff ####################
+# Location in which the build directory should be created
+where         = $(base)
+# The name prefix of the build directory
+builddir      = build
+# The actual build directory name and location
+build         = $(strip $(where)/$(builddir)$(buildsuffix))
 
+# Define the command line args to sub-makes
+MAKEARGS      = -C $(build) -f $(abspath $(makedir)/Makefile) 
+# No available, assured portable utility to compute the relative path between any two absolute paths
+# Hence use absolute path to specify the project root if build directory is elsewhere
+ifneq ($(abspath $(where)),$(abspath $(base)))
+MAKEARGS     += base=$(abspath $(CURDIR))
+endif
 
 .PHONY: all driver physics libs clean clean_driver clean_physics clean_libs again test docs doxygen
 
-all driver physics libs clean clean_driver clean_physics clean_libs again: setup
+all driver physics libs clean clean_driver clean_physics clean_libs again: $(build)
 	@$(MAKE) $(MAKEARGS) $@
 
 realclean:
@@ -21,8 +33,7 @@ docs:
 doxygen:
 	@cd $(docs) && $(DOXYGEN) $(docs)/Doxyfile
 
-setup: $(build)
-
 $(build):
-	@mkdir -p $(@)
+	@echo "Creating build directory $@"
+	@mkdir -p $@
 
--- compile/srcdirs.mk
+++ compile/srcdirs.mk
@@ -5,42 +5,37 @@
 
 ################## Modify only if directory structure changes ####################
 # Top-level directories
-makedir   = $(base)/compile
-driver    = $(base)/src_charm_driver
-physics   = $(base)/src_piny_physics_v1.0
-mathlib   = $(base)/src_mathlib
-topinclude= $(base)/include
-docs      = $(base)/doc
-
-# Build directories
-build     = $(base)/binary
-build_driver = $(build)
-build_physics = $(build)
-build_mathlib= $(build)
+makedir       = $(base)/compile
+driver        = $(base)/src_charm_driver
+physics       = $(base)/src_piny_physics_v1.0
+mathlib       = $(base)/src_mathlib
+topinclude    = $(base)/include
+docs          = $(base)/doc
 
 # Charm driver directory structure
-density   = $(driver)/cp_density_ctrl
-state     = $(driver)/cp_state_ctrl
-largesp   = $(driver)/cp_largesp_ctrl
-fftslab   = $(driver)/fft_slab_ctrl
-loadbal   = $(driver)/load_balance
-main      = $(driver)/main
-ortho     = $(driver)/orthog_ctrl
-paircalc  = $(driver)/paircalc
-strfact   = $(driver)/structure_factor
-uber      = $(driver)/uber
-util      = $(driver)/utility
+density       = $(driver)/cp_density_ctrl
+state         = $(driver)/cp_state_ctrl
+largesp       = $(driver)/cp_largesp_ctrl
+fftslab       = $(driver)/fft_slab_ctrl
+loadbal       = $(driver)/load_balance
+main          = $(driver)/main
+ortho         = $(driver)/orthog_ctrl
+paircalc      = $(driver)/paircalc
+strfact       = $(driver)/structure_factor
+uber          = $(driver)/uber
+util          = $(driver)/utility
 alldriverdirs = $(main) $(density) $(state) $(fftslab) $(loadbal) $(ortho) $(paircalc) $(strfact) $(uber) $(util) $(largesp)
 
 # PINY physics directory structure
-pinymake  = $(physics)/compile
-pinyinc   = $(physics)/include
+pinymake      = $(physics)/compile
+pinyinc       = $(physics)/include
+
 
 # List of modules and libraries to be built (yuck,,,)
-moddriver  := CharmDriver
-modphysics := PinyInterface
-modmath    := MyMathLib
-libdriver  := lib$(moddriver).a
-libphysics := lib$(modphysics).a
-libmath    := lib$(modmath).a
+moddriver    := CharmDriver
+modphysics   := PinyInterface
+modmath      := MyMathLib
+libdriver    := lib$(moddriver).a
+libphysics   := lib$(modphysics).a
+libmath      := lib$(modmath).a
 
