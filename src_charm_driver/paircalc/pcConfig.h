275fad9a960e2c53fe30a5aa4b0ce465caa30c45
Ramprasad Venkataraman <ramv@illinois.edu>
2010-02-18 16:53:52 -0600

    Some eye candy docs for pc::pcConfig

--- src_charm_driver/paircalc/pcConfig.h
+++ src_charm_driver/paircalc/pcConfig.h
@@ -9,10 +9,16 @@ namespace cp {
 /**
  * Dumb structure that holds all the configuration inputs required for paircalc 
  * instantiation, functioning and interaction.
+ *
+ * Almost all of these are invariant through the lifetime of the PC array.
+ * Notably, booleans used in if conditions scattered throughout the code are known at instantiation time.
+ * In the perfect future, values like message priorities and branching factors could change adaptively during the run.
+ * Perhaps, even the decomposition values (grain sizes etc.)
  */
 class pcConfig
 {
     public:
+        //-------------------- Vars that determine the type of the PC array -----------------------
         /// Is this a minimization or dynamics run
         bool isDynamics; ///< @note: This could turn into an enum as more run modes are introduced
         /// Is this a symmetric or asymmetric paircalc instance
@@ -20,13 +26,14 @@ class pcConfig
         /// If this is a symmetric instance, should it use phantom chares to balance the BW path
         bool arePhantomsOn; ///< @note: Originally, phantomSym
 
+
+        //-------------------- Vars that indicate problem size / decomposition --------------------
         /// The total number of planes in the system
         int numPlanes; ///< @warning: Not used in the original code. WHY?
         /// The total number of states in the system
         int numStates;
         /// The number of chunks (4th dimension of decomposition)
         int numChunks; ///< @note: Originally, blkSize
-
         /// The grain size along the states dimensions (plural) (number of states per PC chare)
         int grainSize;
         /** The grain size along the states dimensions for Ortho chares
@@ -37,6 +44,10 @@ class pcConfig
          */
         int orthoGrainSize;
 
+
+        //------------------- Instance, Array, Group identification etc. --------------------------
+        /// The proxyOffset value of thisInstance of OpenAtom computations
+        int instance;
         /// The array ID of the GSpace chare array this instance talks to
         CkArrayID gSpaceAID; ///< @note: Originally, final_callbackid
         /// The entry point to which this instance should send results to
@@ -44,6 +55,8 @@ class pcConfig
         /// The entry point to which this instance should send PsiV tolerance update results to
         int PsiVEP; ///< @note: Originally,  callback_ep_tol
 
+
+        //------------------- Configure the PC array behavior -------------------------------------
         /** The mem footprint vs performance setting for the paircalcs (tribool)
          *
          * -1: dont conserve memory at all
@@ -56,6 +69,19 @@ class pcConfig
         /// Is double-packing on?
         bool isDoublePackOn;
 
+        //------------------- Input communication configs -----------------------------------------
+        /// Will the input data be multicast to PC sections or sent directly (p2p)
+        bool isInputMulticast; ///< @note: Originally, usePairDirectSend
+        /// The priority (set by GSpace) of the input messages
+        int inputMsgPriority; ///< Originally, PairCalcID::priority
+
+        //------------------- Output communication configs ----------------------------------------
+        /// Should the results from each PC chare be reduced or delivered individually to GSpace?
+        bool isOutputReduced; ///< @note: Originally, !gSpaceSum
+        /// If shouldDelayBWsend, what priority should this instance use for the result msgs
+        int resultMsgPriority; ///< @note: Originally,  gpriority
+
+        //------------------- Backward path configs -----------------------------------------------
         /// Should this instance collect result fragments and send them out together in the BW path?
         bool areBWTilesCollected; ///< @note: Originally, collectAllTiles
         /// Should this instance stream the result fragments in the BW path as they become ready?
@@ -64,16 +90,6 @@ class pcConfig
         bool isBWbarriered; ///< @note: Originally, useBWBarrier
         /// Should we tweak msg priority to delay msgs to GSpace carrying the results?
         bool shouldDelayBWsend; ///< @note: Originally, delaybw
-        /// Will the input data be multicast to PC sections or sent directly (p2p)
-        bool isInputMulticast; ///< @note: Originally, usePairDirectSend
-        /// Should the results from each PC chare be reduced or delivered individually to GSpace?
-        bool isOutputReduced; ///< @note: Originally, !gSpaceSum
-        /// The priority (set by GSpace) of the input messages
-        int inputMsgPriority; ///< Originally, PairCalcID::priority
-        /// If shouldDelayBWsend, what priority should this instance use for the result msgs
-        int resultMsgPriority; ///< @note: Originally,  gpriority
-        /// The proxyOffset value of thisInstance of OpenAtom computations
-        int instance;
 
         /**{
          * BGL's painful NIC forces us to split long computations. Configure GEMM splitting here 
