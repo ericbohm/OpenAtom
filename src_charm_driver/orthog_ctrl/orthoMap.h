a40344856546315620bb35a465a4c63a86277ca3
Ramprasad Venkataraman <ramv@illinois.edu>
2010-03-16 23:00:19 -0500

    Move OrthoHelperMap to a different source file

--- src_charm_driver/orthog_ctrl/orthoMap.h
+++ src_charm_driver/orthog_ctrl/orthoMap.h
@@ -48,5 +48,49 @@ class OrthoMap : public CkArrayMapTable2
         }
 };
 
+
+
+
+/// Map group for placing OrthoHelper chares
+class OrthoHelperMap : public CkArrayMapTable2 {
+    public:
+        OrthoHelperMap(MapType2 map, UberCollection _instance)
+        {
+            thisInstance=_instance;
+            #ifdef USE_INT_MAP
+                maptable = new MapType2(map);
+            #else
+                maptable= &OrthoHelpermaptable;
+            #endif
+        }
+
+        ~OrthoHelperMap() { }
+
+        void pup(PUP::er &p)
+        {
+            CkArrayMapTable2::pup(p);
+            #ifdef USE_INT_MAP
+                p|*maptable;
+            #else
+                maptable= &OrthoHelpermaptable;
+            #endif
+        }
+
+        inline int procNum(int, const CkArrayIndex &iIndex)
+        {
+            int *index=(int *) iIndex.data();
+            int proc;
+            #ifdef USE_INT_MAP
+                proc=maptable->get(index[0],index[1]);
+            #else
+                proc=maptable->get(intdual(index[0],index[1]));
+            #endif
+            if(fakeTorus)
+                return(proc%CkNumPes());
+            else
+                return(proc);
+        }
+};
+
 #endif // ORTHO_MAP_H
 
