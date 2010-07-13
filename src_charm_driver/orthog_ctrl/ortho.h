ericbohm/OpenAtom

commit e329764f5124a96c9af43f535695b6b8609f2ade

WIP: Introdce internalType into code that was overlooked earlier
The S matrix generated in the paircalc fw path was received as a double matrix
instead of internalType. A routine to calculate the max diagonal element in this
matrix was also only friendly to doubles. Fix these to process complex correctly too.

Had overlooked a factor of 2 (pcDataSizeFactor) in the bw path. Fixed this too

src_charm_driver/orthog_ctrl/ortho.h
@@ -159,7 +159,7 @@ class Ortho : public CBase_Ortho
         void gamma_done();
 
         /// S should be equal to 2I. This returns max value of deviation from that in this ortho's portion of the S matrix. 
-        inline double array_diag_max(int sizem, int sizen, double *array);
+        inline double array_diag_max(int sizem, int sizen, internalType *array);
         /// Called on ortho(0,0). Checks if PsiV update is needed based on the max deviation in S received via a redn across all orthos. Notifies GSpaceDriver if so. Called periodically in start_calc only for dynamics 
         void maxCheck(CkReductionMsg *msg);
         /// Once all GSpaceDriver chares are notified, they resume Ortho execution via a redn broadcast to this method
@@ -285,35 +285,37 @@ inline void Ortho::print_results(void)
 /**
  * OrthoT tolerance check util return max value
  */
-inline double Ortho::array_diag_max(int sizem, int sizen, double *array)
+inline double Ortho::array_diag_max(int sizem, int sizen, internalType *array)
 {
     double absval, max_ret;
     if(thisIndex.x!=thisIndex.y)
     { //not diagonal
-        max_ret=fabs(array[0]);
+        max_ret = abs(array[0]);
         for(int i=0;i<sizem;i++)
         {
             for(int j=0;j<sizen;j++)
             {
-                absval=fabs(array[i*sizen+j]);
+                absval = abs(array[i*sizen+j]);
                 max_ret = (max_ret>absval) ? max_ret : absval;
             }
         }//endfor
     }
     else
     { //on diagonal 
-        absval=fabs(fabs(array[0]-2.0));
+        absval = abs(array[0]-2.0);
         max_ret = absval;
         for(int i=0;i<sizem;i++)
         {
             for(int j=0;j<sizen;j++)
             {
-                absval=fabs(array[i*sizen+j]);
                 if(i!=j)
+                {
+                    absval = abs(array[i*sizen+j]);
                     max_ret = (max_ret>absval) ? max_ret : absval;
+                }
                 else
                 {
-                    absval=fabs(absval-2.0);
+                    absval = abs(absval-2.0);
                     max_ret = (max_ret>absval) ? max_ret : absval;
                 }//endif
             }

src_charm_driver/orthog_ctrl/ortho.C
@@ -166,7 +166,7 @@ void Ortho::start_calc(CkReductionMsg *msg){
       }//endif
     }
   got_start = true;
-  double *S = (double*) msg->getData();
+  internalType *S = (internalType*) msg->getData();
 #ifdef _NAN_CHECK_
   for(int i=0;i<msg->getSize()/sizeof(double) ;i++)
     {
@@ -195,12 +195,12 @@ void Ortho::start_calc(CkReductionMsg *msg){
       */
       // simple out of place scheme
 
-      double *dest= (double*) omsg->getData();;
-      double tmp;
+      internalType *dest= (internalType*) omsg->getData();;
+      internalType tmp;
       for(int i = 0; i < m; i++)
-	for(int j = 0; j < n; j++){
-	  dest[j * m + i] = S[i *n + j];
-	}
+          for(int j = 0; j < n; j++)
+              dest[j * m + i] = S[i *n + j];
+
       thisProxy(thisIndex.y,thisIndex.x).start_calc(omsg);
 
     }

src_charm_driver/paircalc/ckPairCalculator.C
@@ -1367,7 +1367,7 @@ void PairCalculator::multiplyResult(multiplyResultMsg *msg)
     #endif
 
     // default DGEMM for non streaming comp case
-    int m_in=numPoints*2;   // rows of op(A)==rows C
+    int m_in = numPoints * pcDataSizeFactor;   // rows of op(A)==rows C
     // TODO: is n_in grainSizeX or is k_in?
     int n_in=grainSizeY;     // columns of op(B)==columns C
     int k_in=grainSizeX;     // columns op(A) == rows op(B)

commit 389655352200710ed73ab053091e9db9999b88a3

complex-pc: Fix bug in Ortho::array_diag_max
Used wrong variable in computing absolute value.
Bug introduced in commit e329764f5124a9

src_charm_driver/orthog_ctrl/ortho.h
@@ -302,8 +302,7 @@ inline double Ortho::array_diag_max(int sizem, int sizen, internalType *array)
     }
     else
     { //on diagonal 
-        absval = abs(array[0]-2.0);
-        max_ret = absval;
+        max_ret = abs(array[0]-2.0);
         for(int i=0;i<sizem;i++)
         {
             for(int j=0;j<sizen;j++)
@@ -315,7 +314,7 @@ inline double Ortho::array_diag_max(int sizem, int sizen, internalType *array)
                 }
                 else
                 {
-                    absval = abs(absval-2.0);
+                    absval = abs(array[i*sizen+j]-2.0);
                     max_ret = (max_ret>absval) ? max_ret : absval;
                 }//endif
             }