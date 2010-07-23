f6cd753d2660cbd3ab89439c9f6f27aa74e8a02e
Ramprasad Venkataraman <ramv@illinois.edu>
2010-07-23 11:27:07 -0500

    Copy msg before handing it over to charm and not after.
    Fix crash on pe>1 as trying to memcpy from a msg thats been given to the runtime is not safe.

--- src_charm_driver/cp_density_ctrl/CP_Rho_RealSpacePlane.C
+++ src_charm_driver/cp_density_ctrl/CP_Rho_RealSpacePlane.C
@@ -2398,15 +2398,9 @@ void CP_Rho_RealSpacePlane::doMulticast(){
       if(config.useCommlibMulticast){ComlibBegin(realSpaceSectionCProxy);}
 #endif
        */
-       ProductMsg *loopm=msg;
        for(int kp=0;kp<config.UberJmax;kp++)
 	 {
-	   if(config.useCommlibMulticast){
-	     realSpaceSectionCProxyA[kp].acceptProduct(loopm);
-	   }else{
-	     realSpaceSectionProxyA[kp].acceptProduct(loopm);
-
-	   }//enddif
+       ProductMsg *loopm;
 	   if(kp+1<config.UberJmax)
 	     {
 	       
@@ -2418,6 +2412,14 @@ void CP_Rho_RealSpacePlane::doMulticast(){
 	       loopm->subplane   = thisIndex.y;
 	       memcpy(loopm->data, msg->data, msg->datalen* sizeof(double));
 	     }
+       else
+        loopm=msg;
+	   if(config.useCommlibMulticast){
+	     realSpaceSectionCProxyA[kp].acceptProduct(loopm);
+	   }else{
+	     realSpaceSectionProxyA[kp].acceptProduct(loopm);
+
+	   }//enddif
 	 } //endfor kp
       /*
 #ifdef OLD_COMMLIB
