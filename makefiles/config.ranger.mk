716cfb778a297c59af9c1cd0477ba785117ec2f8
Abhinav Bhatele <bhatele@jaguarpf-login2.ccs.ornl.gov>
2010-10-08 01:15:10 -0400

    removed -DCMK_OPTIMIZE=1 from makefiles

--- makefiles/config.ranger.mk
+++ makefiles/config.ranger.mk
@@ -21,7 +21,7 @@ FFT_HOME      = $(TACC_FFTW2_DIR)
                OPT       = -O3
                # What flags do we use when compiling the fragile portions of piny
                OPT_CARE  = -O2
-               CPPFLAGS += $(DUAL_FFTW) -DFORTRANUNDERSCORE -DCMK_OPTIMIZE=1 \
+               CPPFLAGS += $(DUAL_FFTW) -DFORTRANUNDERSCORE \
                           -I$(FFT_HOME)/include -I$(CHARMBASE)/include/fftlib 
                FFLAGS   += $(OPT)
                CFLAGS   += $(OPT)
