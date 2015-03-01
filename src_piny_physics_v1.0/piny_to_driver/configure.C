7e4e1f24a1bb16ff57919497601032347e18a3c6
Nikhil Jain <nikhil.jain@acm.org>
2015-02-28 20:57:57 -0600

    Fix typo in error checking leading to incorrect failure
    Change-Id: I17ed5dd83c3154540557f859129eaf60e974c40e

--- src_piny_physics_v1.0/piny_to_driver/configure.C
+++ src_piny_physics_v1.0/piny_to_driver/configure.C
@@ -3164,7 +3164,7 @@ void Config::Finale(int nkf1,int nkf2,int nkf3,int nplane_x,int nplane_x_rho, in
   }//endif
 
 
-  if((gemmSplitFWm %2 !=0) || gemmSplitFWm > sGrainSize){
+  if((gemmSplitFWm % 2 !=0) || gemmSplitFWm > sGrainSize){
     PRINTF("   @@@@@@@@@@@@@@@@@@@@_error_@@@@@@@@@@@@@@@@@@@@\n");
     PRINTF("   gemmSplitFWm %d must be an even number less than sGrainSize %d !\n",
         gemmSplitFWm, sGrainSize);
@@ -3172,7 +3172,7 @@ void Config::Finale(int nkf1,int nkf2,int nkf3,int nplane_x,int nplane_x_rho, in
     EXIT(1);
   }//endif
 
-  if((gemmSplitBW %2 !=0) || gemmSplitBW > sGrainSize){
+  if((gemmSplitBW % 2 !=0) || gemmSplitBW < sGrainSize){
     PRINTF("   @@@@@@@@@@@@@@@@@@@@_error_@@@@@@@@@@@@@@@@@@@@\n");
     PRINTF("   gemmSplitBW %d must be an even number greater than sGrainSize %d !\n",
         gemmSplitBW, sGrainSize);
