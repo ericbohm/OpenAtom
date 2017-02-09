4c380c403c612cd58b862bd4d0e1deea4541d0cb
Subhasish Mandal <subhasish.mandal@yale.edu>
2017-02-09 12:52:01 -0500

    bug fixed in Bare Exch

--- src_charm_driver/gw_bse/serial_yale/FORTRAN/Sigma/Bare_Exch/sigma.f90
+++ src_charm_driver/gw_bse/serial_yale/FORTRAN/Sigma/Bare_Exch/sigma.f90
@@ -1,6 +1,7 @@
 !  Sigma_x main program
 !
-!  Dec 2016  subhasish.mandal@yale.edu
+!  Sep. 2014  minjung.kim@yale.edu
+!  July 2015  subhasish.mandal@yale.edu
 
 program sigma
 
@@ -29,11 +30,14 @@ program sigma
    integer :: ndata ! number of g vector in epsilon matrix   
    integer :: ngdata ! number of g vector in epsilon matrix   
    integer, allocatable :: idx(:,:)
+   !integer, allocatable :: ndata, nfft ! number of g vector in epsilon matrix   
    integer, allocatable :: gidx(:) ! link G vectors between polarizability and epsilon
    integer :: Uvec(3) ! Umklapp vector
+   !integer :: gvec(3,:) ! g vectors
    complex(dp), allocatable :: fftbox(:,:,:)
    integer, allocatable :: gvec(:,:) ! g vectors
    integer:: i,j, idata, ii
+   !complex(dp) :: Dgg
    complex(dp), dimension(:), allocatable ::a_n
    complex(dp), dimension(:), allocatable ::a_v
    real(dp), dimension(:), allocatable ::coulb
@@ -50,14 +54,13 @@ real(dp) :: g2, gp2
 !*****************************************
 !*******************
 !******Band info*******
-   nv = 4 ! This is for Silicon example
-   nb = 52 ! This is for Silicon example
+   nv = 4
+   nb = 52
    nc = nb - nv
-   nbmax = 1 ! At present not implemented 
-   nbmin = 1 ! At present not implemented 
+   nbmax = 1
+   nbmin = 1
 !********************
 !********************
-!Some Constants
    Ha2ev=27.211382543519
    Ry2ev=13.60569127
    utest = abs( Uvec(1) ) + abs( Uvec(2) ) + abs( Uvec(3) )
@@ -125,18 +128,17 @@ real(dp) :: g2, gp2
 !!
    print *, 'Allocating a pile-----done---2--'
 !***************************************************************** 
-!Zeroing bunch of variables
 sig_sum=0.0
 sig_g1=0.0
 sig_g2=0.0
+!Brrp%C=0.0
 fr=0.0
 frp=0.0
 fg=0.0
 fgp=0.0
-! Sanity check 
+
    print *, 'norm of k=n=1 in R space is ',sum(abs(psi_R%wk(1)%cg(1:ndata,1))**2)*sys%vol / ndata
 
-   !K-loop starts here
 do ik = 1, nk
 
 sig_g=0.0
@@ -149,13 +151,8 @@ sig_sum=0.0
 fr=0.0
 frp=0.0
 fg=0.0
-fgp=0.
-!band(ib)-loop starts here
-!
-! These are the bands you want to compute for --this is user input but
-!presently resides inside the code 
-!
-do  ib = 1, 2
+fgp=0.0
+do  ib = 1, 5
 
 sig_g=0.0
 sig_g1=0.0
@@ -168,8 +165,7 @@ fr=0.0
 frp=0.0
 fg=0.0
 fgp=0.0
-!q-loop starts here
-!At the end we need to sum up the sigma for all qs. 
+
       
 do iq = 1, nq
 
@@ -195,9 +191,28 @@ fgp=0.0
          !call flush(6)
          !a_v = 0.0d0
 
+deallocate( q%vec )
+call get_qvec( k, q )
+         g_pol%gvec(:,:) = gvec(:, :)
+         ! Reducing g-space --depends on eps_cut
+call reduce_gspace( g_pol, sig_mat%gs(iq), inp, sys, q%vec(1:3,iq), gidx )
+ ngdata = sig_mat%gs(iq)%ng
 
-         ! starts valence bands loop here
+         print*, 'number of data in sigma calculations:', ngdata
+         !call calc_coulb( sys, ngdata, q%vec(:,iq), gvec, coulb, nq )
+call calc_coulb( sys, sig_mat%gs(iq)%ng, q%vec(:,iq), sig_mat%gs(iq)%gvec, coulb, nq )
+deallocate( q%vec )
+call get_qvec( k_, q )
+!         call calc_coulb( sys, ngdata, q%vec(:,iq), gvec, coulb, nq )
+!print*, 'print out coulmb', ik
+
+
+
+
+
          do iv=1, nv
+!         call calc_v(psi_R%wk(ikq),  a_v, FFTsize, sys, Uvec , ndata, iv)
+!         print *, 'called a_v_r', iv
          !print *, 'a_v(1) = ', a_v(1), size(a_v)
          ! print *, 'a_v(1)*a_v(1) = ',a_v(1)*sys%vol/ndata*a_v(1)*sys%vol/ndata
 
@@ -212,18 +227,15 @@ fgp=0.0
            call modify_Uproc_wfn ( ndata, a_v, Uvec, sys, FFTsize )
 
          endif
-         ! Forming fr : fr= (psi_n)*(psi_l) at r
          fr(1:ndata) = a_v(1:ndata) * conjg( a_n(1:ndata) )*sys%vol/ndata
-         ! Forming frp : fr= (psi_n)*(psi_l) at r'
          frp(1:ndata) = a_v(1:ndata) * conjg( a_n(1:ndata) )*sys%vol/ndata
          !print *, 'computed f_r', size(fr), ndata
          !f(1:ndata) = fr(1:ndata)
          !print *, 'computed f_r', fr(1) 
          !print *, 'computed f_rp', frp(1) 
 
-         ! This routine FFTs fr to fg
+         !f_r(1:ndata)=fr(1:ndata)
          call f_r_to_g( -1, fr, fg, FFTsize, ndata )
-         ! if you want to print some fg
 !         print *,'some g,gvec,fg(g)'
 !         do g=1,5
 !           print *,g,gvec(:,g),fg(g)
@@ -234,18 +246,6 @@ fgp=0.0
 
 
 
-! g-space reduction is not implemented here for Bare exachange 
-!         g_pol%gvec(:,:) = gvec(:, :)
-!         if (ik==1 .and. iv==1) then
-!           call reduce_gspace( g_pol, sig_mat%gs(iq), inp, sys, q%vec(1:3,iq), gidx )
-!         endif
-!         ngdata = sig_mat%gs(iq)%ng
-         ngdata = ndata
-         print*, 'number of data in sigma calculations:', ngdata
-         call calc_coulb( sys, ngdata, q%vec(:,iq), gvec, coulb, nq )
-
-!print*, 'print out coulmb', ik
-! ########PRINTING Results STARTS########
 ! !*****************************************
 !print*, 'print out coulmb', ik
 !!!
@@ -259,43 +259,79 @@ write(1,*) '******************************* '
 write(1,*) '******************************* '
 write(1,*) 'start for in =    ', iv
 write(1,*) ' Vcoulb  ABS(D)  sum_sig_g   n1   ig   ga  gb  gc      '
-! We always consider q==1 a special point. At present coulb(g) at q==1 is
-! hardcoded to a particular value for Silicon
+!!
+!!!sig_g=0.0
+!!!sig_g1=0.0
+!!!!!sig_g2=0.0
+!!!sig_sum1=0.0
+!!!sig_sum2=0.0
 if (iq == 1) then
     print*, 'Which q-point you are on : ', q%vec(1:3, iq)
     print*, 'Which k-point you are on : ', k_%vec(1:3, ik)
     do g = 1, ngdata
     coulb(1)=0.38343474
-    sig_g1=  sig_g1  + coulb(g)*real(abs(fg(g)*conjg(fgp(g)))) 
-    write(1, '(3f15.8,610i8)') coulb(g),real(abs(fg(g)*conjg(fgp(g)))),sig_g1, iv, g, gvec(:,g)
+    !sig_g1=  sig_g1  + coulb(g)*real(abs(fg(g)*conjg(fgp(g)))) 
+    sig_g1=  sig_g1  + coulb(g)*real(abs(fg(gidx(g))*conjg(fg(gidx(g)))))
+    !sig_g1=  sig_g1  + coulb(g)*real(abs(fg(gidx(g))*conjg(fgp(gidx(g)))))
+    !write(1, '(3f15.8,610i8)') coulb(g),real(abs(fg(gidx(g))*conjg(fgp(gidx(g))))),sig_g1, &
+    !iv, g, sig_mat%gs(iq)%gvec(:,g)
+    write(1, '(3f15.8,610i8)') coulb(g),real(abs(fg(gidx(g))*conjg(fg(gidx(g))))), sig_g1, iv, g, sig_mat%gs(iq)%gvec(:,g)
+    !write(1, '(3f15.8,610i8)') coulb(g),real(abs(fg(g)*conjg(fgp(g)))),sig_g1, iv, g, gvec(:,g)
+    !write(1,*) 'accumulation of sigma fo iv=: ',iv,  sig_g1+sig_g
     enddo !g ends
+!    enddo !iv ends
     write(1,*) 'accumulation of sigma at q : ', sig_g1
     write(1, '(1f15.8)') sig_g1+sig_g
 elseif (iq .ne. 1 ) then 
+!!else
+!!
 print*, 'Which q-point you are on : ', q%vec(1:3, iq)
 print*, 'Which k-point you are on : ', k_%vec(1:3, ik)
 
-
+!sig_g2=0.00
+!do iv =1, nv
 do g = 1, ngdata 
 
-sig_g2=  sig_g2 + coulb(g)*real(abs(fg(g)*fgp(g))) 
-sig_sum=sig_sum + sig_g
-write(1, '(3f15.8,610i8)') coulb(g),real(abs(fg(g)*conjg(fgp(g)))), sig_g2, iv, g, gvec(:,g)
+!if ( g .ne.1) then 
+!sig_g2=  sig_g2 + coulb(g)*real(abs(fg(g)*fgp(g))) 
+sig_g2=  sig_g2 + coulb(g)*real(abs(fg(gidx(g))*conjg(fg(gidx(g)))))
+!sig_sum=sig_sum + sig_g
+!write(1, '(3f15.8,610i8)') coulb(g),real(abs(fg(gidx(g))*conjg(fgp(gidx(g))))), sig_g2, &
+!iv, g, sig_mat%gs(iq)%gvec(:,g)
+write(1, '(4f15.8,610i8)') coulb(g),real(abs(fg(gidx(g))*conjg(fg(gidx(g))))), &
+coulb(g)*real(abs(fg(gidx(g))*conjg(fg(gidx(g))))), sig_g2, iv, g, sig_mat%gs(iq)%gvec(:,g)
+!write(1, '(3f15.8,610i8)') coulb(g),real(abs(fg(g)*conjg(fgp(g)))), sig_g2, iv, g, gvec(:,g)
 enddo
+!enddo !iv 
 write(1,*) 'accumulation of sigma at q : '
 write(1, '(1f15.8)') sig_g2
 endif
-
-
+!!sig_sum=sig_sum1+sig_sum2
+! !!******************************************
+!
+!
+!
+! enddo  ! end of iq
+!do iq=1, nq
+!sig_sum= sig_g1 + sig_g2 + sig_g
+!enddo
+!sig_sum(ik)=sig_g1(ik)+sig_g2(ik)
+!
+!
+!
+!
+!
 enddo !end of iv
-
 enddo !end of iq
 
 
 do iq=1, nq
-sig_sum= sig_g1 + sig_g2 + sig_g
+sig_sum= sig_g1 + sig_g2 
+!sig_sum= sig_g1 + sig_g2 + sig_g
 enddo
-print*, 'print sigma over all q at k', k_%vec(1:3, ik), '::', -sig_sum, -sig_sum*Ha2eV
+! enddo   ! end of iband
+!enddo
+print*, 'print sigma over all q at k', k_%vec(1:3, ik), '::', -sig_sum, -sig_sum*Ry2eV
 write(1,*) 'band index ', ib, 'K-point ', k_%vec(1:3, ik)
 write(2,*)  ' Dealing with K-point', k_%vec(1:3, ik)
 write(2,*)  '****************'
@@ -303,6 +339,9 @@ write(2,*)  'Band index, Sigma'
 write(1, '(2f15.8)' ) -sig_sum, -sig_sum*Ry2eV
 write(2, '(i8,2f15.8)' ) ib,  -sig_sum*Ry2eV
 
+!enddo
+
+!print*, 'number of data in sigma calculations:', ngdata
 enddo ! end of iband
 !!
 enddo
@@ -312,5 +351,7 @@ close(2)
 
 
 deallocate(a_n)
-
+!!
+!!
+!!
 end program
